bb075f837d9be439e2f5fa3daaab7aa21a498c9b
minataforest <minataforest@gmail.com>
2022-02-26 21:07:58 +0900

    220226 04 Rust 5.1

--- 04 Rust/hello_cargo/src/main.rs
+++ 04 Rust/hello_cargo/src/main.rs
@@ -1,3 +1,27 @@
+// fn main() {
+//     println!("Hello, world!!");
+// }
+
 fn main() {
-    println!("Hello, world!!");
+    let my_string = String::from("hello world");
+
+    // first_word가 `String`의 슬라이스로 동작합니다.
+    let mut word = first_word(&my_string[..]);
+    println!("{}", word);
+    my_string.clear();
+    println!("{}", word);
+
+
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
