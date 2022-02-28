3b9548aa68f5f3ffff76c7b1966bff3309595ffa
minataforest <minataforest@gmail.com>
2022-02-28 23:11:31 +0900

    220228 04 Rust 5.2

---
+++ 04 Rust/reactangles/src/main.rs
@@ -0,0 +1,16 @@
+
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32
+}
+
+fn main() {
+    let rect1 = Rectangle {length: 50, width: 30};
+    println!("rect is {:#?}", rect1);
+    println!("The area of the rectangle is {} square pixels.", area(&rect1));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.length * rectangle.width
+}
